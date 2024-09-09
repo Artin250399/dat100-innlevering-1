79c5c3391bcef007afd8ed4e37a33a56825d8168
Artin250399 <artinbahrampour1599@gmail.com>
2024-09-09 13:02:44 +0200

    Add files via upload

---
+++ dat100-innlevering-1/uke 37 Oppgave/src/no/hvl/dat100/oppgave02.java
@@ -0,0 +1,29 @@
+package no.hvl.dat100;
+
+public class oppgave02 {
+
+	
+
+	public class OppgaveB6 {
+		public static void main(String[] args) {
+		int i=45362;
+		if (i>= 208050 && i <=292850 ) {
+			System.out.println(0.017* i);	
+
+	}else if (i>= 292851 && i <=670000 ) {
+		System.out.println(0.04* i);
+
+	}else if (i>= 670001 && i <=937900 ) {
+		System.out.println(0.136* i);
+		
+	}else if (i>= 937901 && i <=1350000) {
+		System.out.println(0.166 * i);
+		
+	}else if (i>= 1350001) {
+		System.out.println(0.176* i);
+		
+		
+		
+	}
+	}}
+}
---
+++ dat100-innlevering-1/uke 37 Oppgave/src/no/hvl/dat100/oppgave03.java
@@ -0,0 +1,14 @@
+package no.hvl.dat100;
+
+public class oppgave03 {
+public static void main(String[] args) {
+	int n=12;
+	int fakultet =1;
+	for(int i=1; n>=i; i++) {
+		
+		fakultet *=i;
+		System.out.println(fakultet);
+		
+	}
+}
+}
